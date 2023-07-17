44bd762a581754f01ebd80ba6073c3577d17c75d
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-07-17 15:05:46 +0200

    first commit

---
+++ src/TemplateGeneratorVideo.stories.jsx
@@ -0,0 +1,18 @@
+﻿import {TemplateGenerator} from "./TemplateGenerator.jsx";
+
+
+export default {
+    title: 'Demo/Template',
+    component: TemplateGenerator,
+    tags: ['autodocs'],
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+};
+
+export const Small = {
+    args: {
+        size: 'small',
+        label: 'Button',
+    },
+};
