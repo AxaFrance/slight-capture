59824ff5e9b34576709bac324e36a636ed16408b
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-07-27 18:12:46 +0200

    fix storybook (release)

--- src/SlightCapture/video.js
+++ src/SlightCapture/video.js
@@ -1,10 +1,25 @@
-﻿import cuid from "cuid";
+﻿
 import {base64ToBlob, imageResize, loadImageAsync, toImageBase64} from "./image.js";
 import {findMatch} from "./templateMatching.js";
 import {toBase64Async, zoneAsync} from "./template.js";
 import {detectAndComputeSerializable} from "./match.js";
 import {loadScriptAsync} from "./script.js";
 
+const cuid = () =>{
+    let d = new Date().getTime();//Timestamp
+    let d2 = (performance && performance.now && (performance.now() * 1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
