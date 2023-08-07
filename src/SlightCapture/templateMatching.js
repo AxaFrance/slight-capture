f467e71222bc0b337771ad108469f9f812b92765
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-08-07 16:49:29 +0200

    fix(brightness): adjust too dark

--- src/SlightCapture/templateMatching.js
+++ src/SlightCapture/templateMatching.js
@@ -1,6 +1,6 @@
 ﻿import {imageResize} from "./image.js";
 
-export const autoAdjustBrightness = (cv) => (image, minimumBrightness=0.85, minimumRatio = 0, maximumRatio = 100) => {
+export const autoAdjustBrightness = (cv) => (image, minimumBrightness=0.7, minimumRatio = 0, maximumRatio = 100) => {
     let brightness = 0;
     const src = image;
     if (src.isContinuous()) {
